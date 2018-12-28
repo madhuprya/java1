00fe62317086b493d17a775d06d3de6467779e8a
Nalinshankar <nalinmakaut@gmail.com>
2018-12-28 10:18:01 +0530

    modified

--- src/test/java/com/stackroute/javape3/StudentMarksTest.java
+++ src/test/java/com/stackroute/javape3/StudentMarksTest.java
@@ -1,66 +1,31 @@
-package com.stackroute.javape3;
-
-import org.junit.After;
-import org.junit.Before;
+package com.stackroute;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class StudentMarksTest {
-    StudentMarks student;
 
-    @Before
-    public void setUp() throws Exception {
-    }
+  StudentMarks marks = new StudentMarks();
 
-    @After
-    public void tearDown() throws Exception {
-    }
+  @Test
+  public void test1() {
+      assertEquals("Grade should be between 0 to 100",true, StudentMarks.studentmarks(00));
 
-    @Test
-    public void checkForGrades1() {
-        //act
-        String expected = "O";
-        //arrange
-        String result = student.checkForGrades(5,new int[]{92,96,91,94,98});
-        //assert
-        assertEquals(expected,result);
-    }
+  }
+  @Test
+  public void test2() {
+      assertEquals("Grade is in between 0 and 100",true, StudentMarks.studentmarks(25));
 
-    @Test
-    public void checkForGrades2() {
-        //act
-        String expected ="E";
-        //arrange
-        String result= student.checkForGrades(3,new int[]{81,85,86});
-        //assert
-        assertEquals(expected,result);
-    }
-    @Test
-    public void checkForGrades3() {
-        //act
-        String expected ="Error...please enter positive no students";
-        //arrange
-        String result= student.checkForGrades(0,new int[]{81,85,86});
-        //assert
-        assertEquals(expected,result);
-    }
-    @Test
-    public void checkForGrades4() {
-        //act
-        String expected ="Error...marks for student"+3+"is more than 100";
-        //arrange
-        String result= student.checkForGrades(3,new int[]{81,85,105});
-        //assert
-        assertEquals(expected,result);
-    }
-    @Test
-    public void checkForGrades5() {
-        //act
-        String expected ="Error...marks for student"+3+"is less than 0";
-        //arrange
-        String result= student.checkForGrades(3,new int[]{81,85,0});
-        //assert
-        assertEquals(expected,result);
-    }
-}
+  }
+
+  @Test
+  public void test3() {
+      assertEquals(  "grade should be between 0 to 100", false, StudentMarks.studentmarks(200));
+  }
+
+  @Test
+  public void test4() {
+      assertNotEquals("grade should be equal to 0 to 100", true, StudentMarks.studentmarks(450));
+  }
+
+
+  }
